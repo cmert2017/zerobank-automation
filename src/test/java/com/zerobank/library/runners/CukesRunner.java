5911a776e9e097461205d92cef43010977d4996a
cmert2017 <cihanmert2075@gmail.com>
2021-01-17 12:57:48 -0500

    Initial commit

---
+++ src/test/java/com/zerobank/library/runners/CukesRunner.java
@@ -0,0 +1,22 @@
+package com.zerobank.library.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/default-cucumber-reports",
+                "rerun:target/rerun.txt",
+                "json:target/cucumber.json"
+        },
+
+        features = "src/test/resources/features",
+        glue = "com/zerobank/library/stepdefinitions",
+        dryRun = false,
+        tags = "@sample1"
+
+)
+public class CukesRunner {
+}
